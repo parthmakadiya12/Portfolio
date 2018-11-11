533b5e57c3a826a3e08b0df61fc0c086435a32f4
Parth Makadiya <parthmakadiya12@gmail.com>
2018-11-11 01:05:58 +0100

    New Design changes

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function clickHandle() {
-    alert('Hello');
+function menuBar() {
+  let  navbar = document.querySelector(".navbar");
+  let uList= document.querySelector(".list");
+  console.log(navbar);
+  if (navbar.className === "navbar") {
+      navbar.className += " responsive";
+      uList.className+=" uResponsive";
+  }
+  else{
+    navbar.className = "navbar";
+    uList.className ="list";
+  }
 }
