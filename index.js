fb10e46a8ddff2b7ada9a3e2166871783699138d
Parth Makadiya <parthmakadiya12@gmail.com>
2018-11-11 23:02:35 +0100

    design updates page2

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function menuBar() {
     uList.className ="list";
   }
 }
+
+let  circle = document.querySelector(".circle");
+  circle.style.strokeDasharray=30;
+
+
+
+var canvas = document.getElementById('myCanvas');
+// var context = canvas.getContext('2d');
+// context.beginPath();
+// //	context.arc(x,y,r,sAngle,eAngle,counterclockwise);
+// function progress(){
+//   for(var i=0;i<10;i++){
+//     context.arc(256,256,100,i*0.1*Math.PI,1.5*Math.PI,0);
+//     context.strokeStyle='#ffffff';
+//     context.lineWidth=10;
+//     context.stroke(); 
+//     context.
+//   }
+// }
+
+// progress();
